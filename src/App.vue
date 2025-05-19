b0e251267d5bb9e51850e2bd286b92e229ab97e2
AbhishekKr-Jha <akjha4127@gmail.com>
2025-05-20 00:05:36 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+
+
+
+<script>
+import Card from './components/Card.vue';
+import VueProjectList from './components/VueProjects.js'
+// import { VueProjectList } from './components/VueProjects';
+
+export default{
+  name:'App',
+  data(){
+    return{
+      VueProjectList
+    }
+  },
+  components:{
+    Card
+  },
+  
+}
+</script>
+
+<template>
+
+
+
+<div style="max-width: 1300px;background-color: black;padding-bottom: 20px;" class="w-full  mx-auto flex items-center justify-center" >
+
+<div style="gap: 30px;" class="w-full flex flex-col items-center  mt-60">
+
+  <p style="font-size: 48px;">Web Apps</p>
+
+  <div class="w-full flex flex-wrap items-center justify-center gap-60">
+<Card  v-for="ele in VueProjectList" :key="ele.project_name" :cardDetails="ele" />
+  </div>
+
+</div>
+
+</div>  
+
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+
+
+<script>
+
+export default{
+    name:'card',
+    props:{
+        cardDetails:Object
+    }
+}
+</script>
+
+
+
+
+
+<template>
+    <div class="card relative">
+        <div style="height: 150px; padding: 6px;" class="w-full ">
+            <img :src="cardDetails?.image_link" class="w-full h-full object-fit-contain rounded-lg"  alt="load...">
+        </div>
+        <div style="padding: 10px;padding-top: 0;gap: 0px;" class="w-full flex flex-col  ">
+
+            <p style="font-size: 44px;" class="text-red font-medium  text-primary">{{cardDetails?.project_name}}  </p>
+            <p style="font-size: 20px;margin-top: 5px;" class="text-center w-full  "> - {{cardDetails?.project_type}}  - </p>
+ 
+<div style="margin-top: 20px;" class="flex flex-col gap-10 ">
+    <p class="text-lg">{{ cardDetails.description.line1 ? `• ${cardDetails.description.line1}` : ' • • • ' }}</p>
+    <p class="text-lg">{{ cardDetails.description.line2 ? `• ${cardDetails.description.line2}` : ' • • • ' }}</p>
+    <p class="text-lg text-center "> {{ cardDetails.description.line3 ? `• ${cardDetails.description.line3}` : ' • • • ' }}</p>
+</div>
+
+
+
+            <div style="gap: 30px;" class="w-full absolute bottom-15 left-0 flex justify-center items-center  "> 
+                <a :href="cardDetails?.github_link" target="_blank" class=" block"><i style="font-size: 30px;" class="fa-brands fa-github "></i></a>
+                <a :href="cardDetails?.hosting_link" target="_blank" class=" block"><i style="font-size: 30px;"  class="fa-solid fa-eye text-2xl"></i></a>
+            </div>
+
+        </div> 
+
+    </div>
+</template>
